0290e5f2a0daf860fca2a69079c5bdbe51ce1fff
thechozenwan <turynlb@gmail.com>
2021-03-15 14:39:11 +0200

    Initial Commit

---
+++ Particle_Dio/src/Particle_Dio.ino
@@ -0,0 +1,19 @@
+/*
+ * Project Particle_Dio
+ * Description: Read Digital Input and forward to the commander dashboard
+ * Author: Turyn
+ * Date: 15 March 2021
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  digitalWrite();
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
